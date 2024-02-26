4e70fa34dbb732a90fa3fe13f2b649df834685f8
Hà Quang Hưng <hungha15123@gmail.com>
2024-02-27 00:12:24 +0700

    create rawfoodnutrient migration

---
+++ database/migrations/2024_02_26_164812_create_raw_food_nutrients_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('raw_food_nutrients', function (Blueprint $table) {
+            $table->id();
+            $table->double('Water');
+            $table->double('Protein');
+            $table->double('Total_lipid_(fat)');
+            $table->double('Carbohydrate_by_difference');
+            $table->double('Calcium_Ca');
+            $table->double('Iron_Fe');
+            $table->double('Magnesium_Mg');
+            $table->double('Phosphorus_P');
+            $table->double('Potassium_K');
+            $table->double('Sodium_Na');
+            $table->double('Zinc_Zn');
+            $table->double('Copper_Cu');
+            $table->double('Manganese_Mn');
+            $table->double('Selenium_Se');
+            $table->double('Vitamin_C_total_ascorbic_acid');
+            $table->double('Thiamin');
+            $table->double('Riboflavin');
+            $table->double('Niacin');
+            $table->double('Pantothenic_acid');
+            $table->double('Vitamin_B-6');
+            $table->double('Folate_total');
+            $table->double('Vitamin_B-12');
+            $table->double('Vitamin_A_RAE');
+            $table->double('Cholesterol');
+            $table->double('Fatty_acids_total_saturated');
+            $table->double('Fatty_acids_total_monounsaturated');
+            $table->double('Fatty_acids_total_polyunsaturated');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('raw_food_nutrients');
+    }
+};
